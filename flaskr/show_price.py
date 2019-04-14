5c6ce15ca37bf2b3614323664dfa3f3bd6f43f43
rockmkd <rockmkd@gmail.com>
2019-04-14 23:40:31 +0900

    init

---
+++ flaskr/show_price.py
@@ -0,0 +1,86 @@
+import requests
+import json
+import pprint
+import math
+
+def getData(id) :
+
+    moreData = True
+    page = 1
+    result = []
+    while( moreData ):
+        url = f"https://new.land.naver.com/api/articles/complex/{id}?tradeType=A1&page={page}"
+        response = requests.get(url)
+        data = json.loads(response.text)        
+        result = result + data['articleList']
+
+        if data['isMoreData'] :
+            page = page + 1
+        else :
+            moreData = False    
+    return result    
+
+# {
+#     key: 'builing + floor + price',
+#     price: 
+#     floor:
+#     building:
+#     size:
+#     desc: 
+# }
+
+
+def dedupe(data) :
+    dedupedData = []
+    for article in data:
+        key = article['buildingName'] + article['floorInfo'] + article['dealOrWarrantPrc']
+        el = [x for x in dedupedData if x['key'] == key]
+        if len(el) == 0 :
+            dedupedData.append({
+                'key': key,
+                'price': strToNum(article['dealOrWarrantPrc']),
+                'just_price': article['dealOrWarrantPrc'],
+                'floor': article['floorInfo'],
+                'building': article['buildingName'],
+                'size': article['areaName'],
+                'desc': article.get('articleFeatureDesc', '')
+            })
+    return dedupedData
+
+def sizeFilter(data, sizeList) :
+    filterdData = []
+    for article in data: 
+        if article['size'] in sizeList:
+            filterdData.append(article)
+    
+    return filterdData
+
+def strToNum(str):
+    arr = str.replace(",","").replace(" ","").split("억")
+    
+    if arr[1] == "" : 
+        arr[1] = "0"
+
+    return 10000 * int(arr[0]) + int(arr[1])
+        
+def pretty_print(data):
+    print (f" {data['size']}m2 {data['building']} {data['floor']}: {money_format(data['price'])} - {data['desc']} ")
+
+def money_format(price):
+    result = str(math.floor(price / 10000)) + "억"
+    result = result +  ( "" if (price % 10000) == 0 else str((price % 10000)) + "만")
+    return result
+
+with open('config.json') as f:
+    configs = json.load(f)
+
+def main(): 
+    for config in configs:
+        result = sizeFilter(dedupe(getData(config['id'])), config['size'])
+        sortedResult = sorted(result,  key=lambda k: k.get('price', 0), reverse=False)
+
+        print( f"======= {config['name']} =========")
+        for article in sortedResult:
+            pretty_print(article)
+    
+main()
